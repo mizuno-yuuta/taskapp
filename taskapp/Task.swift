f1b1e9430eb35e12e7e395de8ea91585ea2af8c0
yuuta.mizuno <f0b325382t65j6a@icloud.com>
2020-01-27 13:16:34 +0900

    要修正

---
+++ taskapp/Task.swift
@@ -0,0 +1,23 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 水野優太 on 2020/01/26.
+//  Copyright © 2020 yuuta.mizuno. All rights reserved.
+//
+
+ import RealmSwift
+class Task: Object{
+    //管理用　ID。　プライマリーキー
+    @objc dynamic var id = 0
+    //タイトル
+    @objc dynamic var title = ""
+    //内容
+    @objc dynamic var contents = ""
+    // 日常
+    @objc dynamic var date = Date()
+    //id をプライマリーキーとして設定
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
